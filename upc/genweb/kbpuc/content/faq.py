19b2cdffe58fe6628ab89ef0448d82917d4e14f7
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-04-01 08:46:58 +0000

    campos faq
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.kbpuc@944 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genweb/kbpuc/content/faq.py
+++ upc/genweb/kbpuc/content/faq.py
@@ -25,8 +25,22 @@ from upc.genweb.kbpuc.config import PROJECTNAME
 from Products.ATContentTypes.content.document import ATDocumentSchema, ATDocument
 
 from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import ReferenceBrowserWidget
+from AccessControl import ClassSecurityInfo
 
 faq_kbpuc_Schema = ATDocumentSchema.copy() + atapi.Schema((
+
+     atapi.LinesField('servicios',
+                required=False,
+                vocabulary='listaServicios',
+                enforceVocabulary=True,
+                widget=atapi.InAndOutWidget(
+                        label="Lista de Servicios",
+                        label_msgid="label_custom_view_fields",
+                        description="Select which fields to display when "
+                        "'Display as Table' is checked.",
+                        description_msgid="help_custom_view_fields",
+                        i18n_domain = "upc.genweb.kbpuc"),
+     ),
 
 ))
 
@@ -39,6 +53,22 @@ class Faq(ATDocument):
     schema = faq_kbpuc_Schema
 
     implements(IFaq)
+
+    security = ClassSecurityInfo()
+
+    security.declareProtected(View, 'listaServicios')
+    def listaServicios(self):
+        """Return a list of metadata fields from portal_catalog.
+        """
+        portal_catalog = getToolByName(self, 'portal_catalog')
+        mt = portal_catalog.searchResults(portal_type = 'Servei',sort_on='Date',review_state='published')
+        new_list=[]
+        for f in mt:
+            new_list.append(f.Title)
+        new_list.sort()
+        return new_list
+
+
 
 atapi.registerType(Faq, PROJECTNAME)
 

779bb5c4c59780dec358fc85ec061239cd2e0d6c
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-04-01 09:40:17 +0000

    info general content types fields
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.kbpuc@945 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genweb/kbpuc/content/infogeneral.py
+++ upc/genweb/kbpuc/content/infogeneral.py
@@ -21,6 +21,7 @@ from Products.validation import V_REQUIRED
 from upc.genweb.kbpuc import kbpucMessageFactory as _
 from upc.genweb.kbpuc.interfaces import IInfoGeneral
 from upc.genweb.kbpuc.config import PROJECTNAME
+from zope.component import getMultiAdapter, getUtility
 
 from Products.ATContentTypes.content.document import ATDocumentSchema, ATDocument
 
@@ -28,9 +29,70 @@ from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import Reference
 
 infogeneral_kbpuc_Schema = ATDocumentSchema.copy() + atapi.Schema((
 
+    atapi.TextField('text',
+        required=False,
+        searchable=True,
+        storage = atapi.AnnotationStorage(migrate=True),
+        validators = ('isTidyHtmlWithCleanup',),
+        #validators = ('isTidyHtml',),
+        default_output_type = 'text/x-html-safe',
+        widget = atapi.RichWidget(
+            i18n_domain='upc.genweb.kbpuc',
+            label=_(u'label_long_description', default=u'Contingut'),
+            rows = 25,
+            allow_file_upload = zconf.ATDocument.allow_document_upload),
+        schemata="default",
+    ),
+
+    atapi.LinesField(
+        name='producte',
+        widget=atapi.MultiSelectionWidget(
+            format="select",
+            label = _(u'label_producte', default=u'Producte'),
+            i18n_domain='upc.genweb.kbpuc',
+        ),
+        languageIndependent=True,
+        multiValued=False,
+        schemata="default",
+        vocabulary='getProducteVocabulary',
+        enforceVocabulary = True,
+    ),
+
+    atapi.StringField(
+        name = 'servei',
+        required = False,
+        searchable = False,
+        languageIndependent=True,
+        default = "getServei",
+        widget = atapi.StringWidget(
+            label = _(u'label_servei', default=u'Servei'),
+            i18n_domain='upc.genweb.kbpuc',
+        ),
+        schemata="default",
+    ),
+
+    atapi.StringField(
+        name = 'categoria',
+        required = False,
+        searchable = False,
+        languageIndependent=True,
+        default = "getCategoria",
+        widget = atapi.StringWidget(
+            label = _(u'label_categoria', default=u'Categoria'),
+            i18n_domain='upc.genweb.kbpuc',
+        ),
+        schemata="default",
+    ),
+
 ))
 
 schemata.finalizeATCTSchema(infogeneral_kbpuc_Schema, moveDiscussion=False)
+
+infogeneral_kbpuc_Schema.changeSchemataForField('subject', 'default')
+infogeneral_kbpuc_Schema.moveField('subject', after='title')
+
+infogeneral_kbpuc_Schema.changeSchemataForField('relatedItems', 'default')
+infogeneral_kbpuc_Schema.moveField('relatedItems', after='text')
 
 class InfoGeneral(ATDocument):
     """InfoGeneral KBPUC """
@@ -39,6 +101,20 @@ class InfoGeneral(ATDocument):
     schema = infogeneral_kbpuc_Schema
 
     implements(IInfoGeneral)
+    
+    security = ClassSecurityInfo()
 
+    security.declarePublic('getProducteVocabulary')
+    def getProducteVocabulary(self):            
+        return ['Gestió de personal','Gestió','personal']
+
+    security.declarePublic('getServei')
+    def getServei(self):
+        return self.getParentNode().getParentNode().title
+
+    security.declarePublic('getCategoria')
+    def getCategoria(self):
+        return self.getParentNode().title
+        
 atapi.registerType(InfoGeneral, PROJECTNAME)
 

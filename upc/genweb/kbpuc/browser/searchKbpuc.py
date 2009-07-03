a75178308499a956130ab18c32dc3eaf17bfe14e
pilar.marinas <pilar.marinas@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-07-03 08:18:18 +0000

    Cerca KBPUC
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.kbpuc@1189 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genweb/kbpuc/browser/searchKbpuc.py
+++ upc/genweb/kbpuc/browser/searchKbpuc.py
@@ -11,20 +11,42 @@ class searchKbpuc(BrowserView):
     __call__ = ViewPageTemplateFile('searchKbpuc.pt')   
 
     def datahandler(self, data):
-        portal_catalog = getToolByName(self, 'portal_catalog')
-        mt = portal_catalog.searchResults(portal_type = 'Servei')
+        portal_catalog = getToolByName(self, 'portal_catalog')        
+        mt = portal_catalog.searchResults(portal_type = 'Servei', sort_on='sortable_title', sort_order='ascending')        
 
-        results = []
-        res = []
-
-        for serv in mt:
-            for dt in data:
+        results = []       
+        res = []        
+          
+        for serv in mt:            
+            c = '0'                    
+#            results.append(serv.Title);
+            for dt in data:               
                 if dt.portal_type != 'Servei':
+                        
                      if dt.portal_type == 'Categoria':
-                          if dt.getObject().getParentNode().UID() == serv.getObject().UID():
-                              results.append(dt)
+                          if c == '0':
+                             results.append(serv) 
+                             c = '1' 
+                          if dt.getObject().getParentNode().UID() == serv.getObject().UID():                          
+                             results.append(dt)   
+                          else:
+                              continue                           
                      else:
-                          if dt.getObject().getParentNode().getParentNode().UID() == serv.getObject().UID():
-                              results.append(dt)
-            res.append(results)
+                          if dt.getObject().getParentNode().getParentNode().UID() == serv.getObject().UID():                               
+                              results.append(dt)             
+            #import pdb; pdb.set_trace() 
+            res.append(results)                
+#            res.sort()                           
         return res
+
+#    def serveis(self, data):
+#        portal_catalog = getToolByName(self, 'portal_catalog')
+#        mt = portal_catalog.searchResults(portal_type = 'Servei')    
+#
+#        new_list=[]
+#        for f in mt:
+#            new_list.append(f.Title)
+#        new_list.sort()
+#        
+#        return new_list
+        
